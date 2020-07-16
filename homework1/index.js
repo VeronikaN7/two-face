1c6a1fa744d48334334751c9e1062523ce5eb7c1
Veronika <veronka97@gmail.com>
2020-07-17 00:10:13 +0300

    one more template

--- homework1/index.js
+++ homework1/index.js
@@ -1,49 +1,62 @@
 
 
 const milk = 15.678;
-console.log("Ціна молока - "+milk);
+console.log(milk);
 
 const cacao = 123.965;
-console.log("Ціна какао  - "+cacao);
+console.log(cacao);
 
 const butter = 90.2345;
-console.log("Ціна масла  - "+butter);
+console.log(butter);
 
 //Використовуючи вбудований об'єкт Math – виведіть максимальне число
 const maxMean = Math.max(milk, cacao, butter);
-console.log("Максимальна ціна - "+maxMean);
+console.log(maxMean);
 //Використовуючи вбудований об'єкт Math – виведіть мінімальне число
 const minMean = Math.min(milk, cacao, butter);
-console.log("Мінімальна ціна - "+minMean);
+console.log(minMean);
 //Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму
 const totalPrice = milk + cacao + butter;
-console.log("Вартість всіх товарів - "+totalPrice);
+console.log(totalPrice);
 //Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою.Округлення використовувати в МЕНЬШУ сторону.
 const theWholePrice = Math.floor(milk) + Math.floor(cacao) + Math.floor(butter);
-console.log("Вартість всіх товарів без копійок - "+theWholePrice);
+console.log(theWholePrice);
 //Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300)
 const roundingHundreds = Math.ceil(theWholePrice/100)*100
-console.log("Сума товарів округлена до сотень - "+roundingHundreds);
+console.log(roundingHundreds);
 //Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.
 const startAmount = 500;
 const amountRest = startAmount - totalPrice;
-console.log("Сума решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн - "+amountRest);
+console.log(amountRest);
 //Виведіть середнє значення цін, округлене до другого знаку після коми
 const avaragePrice = (totalPrice/3).toFixed(2)
-console.log("Cереднє значення цін, округлене до другого знаку після коми - "+avaragePrice);
+console.log(avaragePrice);
 //Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?
 const isNumberEven = (theWholePrice%2) === 0;
-console.log("Булеве значення - "+isNumberEven);
+console.log(isNumberEven);
 
 //(Більш складне) Створіть змінну, в якої збережіть випадкову знижку (використовуйте функцію Math.random).
 const price = 200;
 const discount = Math.round(Math.random()*100);
 const afterDiscount = price*discount/100
 const money = 500;
+const toPay = price - afterDiscount
 const cost = price/2;
 const netProfit = (cost - afterDiscount).toFixed(2);
+console.log(price);
+console.log(discount);
+console.log(money);
+console.log(toPay);
+console.log(cost);
+console.log(netProfit);
 
-console.log("Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми.Виведіть чистий прибуток, якщо клієнт заплатив зі знижкою та собівартість товарів рівно в два рази нижче їх ціни? Приклад: ціна була "+ price +" , знижка склала" + discount + " %, клієнт заплатив на " + afterDiscount + "меньше, собівартість = " +  cost + ". Чистий прибуток = " + netProfit);
+console.log(`Ціна: ${price} грн
+Знижка: ${discount} %
+Грошей: ${money} грн
+До сплати: ${toPay} грн
+Собівартість: ${cost} грн
+Чистий прибуток: ${netProfit} грн
+`);
 
 str = `
     <ul>
