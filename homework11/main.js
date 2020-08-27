abe184aec87622c8389c76e9d75c3e2f289ee27d
Veronika <veronka97@gmail.com>
2020-08-27 11:31:25 +0300

    hw11

---
+++ homework11/main.js
@@ -0,0 +1,18 @@
+
+
+
+ function getRandomChinese(length){
+   new Promise((resolve) => {
+    let word =``;
+  const timer =  setInterval(() => {
+    const sign = Date.now()%100000
+    word += String.fromCharCode(sign) ;
+    length--;
+    if(length === 0)
+    clearInterval(timer)
+    console.log(word)
+  },  length * 50);
+  
+})}
+
+getRandomChinese(5)
